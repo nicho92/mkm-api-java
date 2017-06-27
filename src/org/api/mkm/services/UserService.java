47d0096f29ef5f669dde5a3eb6e474b8ab34dbdc
nicho92 <nicolas.pihen@gmail.com>
2017-06-27 14:40:51 +0200

    sendMessage service

--- src/org/api/mkm/services/UserService.java
+++ src/org/api/mkm/services/UserService.java
@@ -1,6 +1,7 @@
 package org.api.mkm.services;
 
 import java.io.IOException;
+import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
@@ -15,6 +16,7 @@ import org.api.mkm.modele.Link;
 import org.api.mkm.modele.Thread;
 import org.api.mkm.modele.Response;
 import org.api.mkm.modele.User;
+import org.api.mkm.modele.WantItem;
 import org.api.mkm.tools.MkmAPIConfig;
 
 import com.thoughtworks.xstream.XStream;
@@ -63,6 +65,53 @@ public class UserService {
 		//String xml= IOUtils.toString(connection.getInputStream(), StandardCharsets.UTF_8);
 		
 	 	return true;
+	}
+	
+	
+	
+	
+	
+	
+	public boolean sendMessage(User u, String message)throws IOException, MkmNetworkException, MkmException
+	{
+		String link="https://www.mkmapi.eu/ws/v2.0/account/messages/"+u.getIdUser();
+		
+		logger.debug("LINK="+link);
+    	HttpURLConnection connection = (HttpURLConnection) new URL(link).openConnection();
+				            connection.addRequestProperty("Authorization", auth.generateOAuthSignature2(link,"POST")) ;
+				       		connection.setDoOutput(true);
+				    		connection.setRequestMethod("POST");
+				    		connection.connect();
+				    		
+		OutputStreamWriter out = new OutputStreamWriter(connection.getOutputStream());
+		StringBuffer temp = new StringBuffer();
+		temp.append("<?xml version='1.0' encoding='UTF-8' ?>");
+		temp.append("<request><message>"+message+"</message>");
+		temp.append("</request>");
+		logger.debug("REQU="+temp);
+		
+		out.write(temp.toString());
+		out.close();
+		MkmAPIConfig.getInstance().updateCount(connection);
+		
+		boolean code= connection.getResponseCode()>=200 && connection.getResponseCode()<300;
+		
+		if(code)
+		{
+			String xml= IOUtils.toString(connection.getInputStream(), StandardCharsets.UTF_8);
+			logger.debug("RESP="+xml);
+			Response res = (Response)xstream.fromXML(xml);
+			if(res.getErrors()!=null)
+				throw new MkmException(res.getErrors());
+			
+			return code;
+		}
+		else
+		{
+			throw new MkmNetworkException(connection.getResponseCode());
+		}
+		
+		
 	}
 	
 	
