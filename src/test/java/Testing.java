841ed221609568dd7ac6ab043851bfa120f4053f
nicho92 <nicolas.pihen@gmail.com>
2018-12-03 14:34:53 +0100

    testing

---
+++ src/test/java/Testing.java
@@ -0,0 +1,32 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+import org.api.mkm.services.OrderService;
+import org.api.mkm.services.OrderService.ACTOR;
+import org.api.mkm.services.OrderService.STATE;
+import org.api.mkm.tools.MkmAPIConfig;
+import org.junit.Test;
+
+public class Testing {
+
+	@Test
+	public void test() throws IOException{
+		
+		Properties p = new Properties();
+		p.load(new FileInputStream(new File("C:\\Users\\Nicolas\\.magicDeskCompanion\\pricers\\MagicCardMarket.conf")));
+		
+		try {
+		MkmAPIConfig.getInstance().init(p);
+		OrderService serv = new OrderService();
+		System.out.println(serv.listOrders(ACTOR.buyer, STATE.received, null));
+		}catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+		
+	}
+
+}
